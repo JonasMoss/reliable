14d63579c3a928fd0dfda938ad990a78e6c83242
Jonas Moss <jonas.gjertsen@gmail.com>
2019-09-03 18:29:16 +0200

    :construction: Changed path of test data.

--- tests/testthat/test_reliability.R
+++ tests/testthat/test_reliability.R
@@ -1,8 +1,8 @@
 context("Reliabiltiy function")
 
-fit_3f = readRDS("data_fit_3f.Rds")
-fit_3f_cov = readRDS("data_fit_3f_cov.Rds")
-fit_3f_orth = readRDS("data_fit_3f_orth.Rds")
+path = system.file("testdata", package = "reliable")
+
+fit_3f_cov = readRDS(paste0(path, "/data_fit_3f_cov.Rds"))
 
 # Does reliabiltiy with no iota return the same as reliabiltiy with the optimal
 # iota?
@@ -14,14 +14,14 @@ expect_equal(reliability(object, type = "sumscore", iota = iota),
 
 ## Does the reliabiltiy equal coefficient H?
 
-fit_1f_orth = readRDS("data_fit_1f_orth.Rds")
+fit_1f_orth = readRDS(paste0(path, "/data_fit_1f_orth.Rds"))
 object = to_list(fit_1f_orth)
 expect_equal(reliability(object)$reliability,
              coefficient_H(object))
 
 ## Does the reliabiltiy equal omega??
 
-fit_1f_orth2 = readRDS("data_fit_1f_orth2.Rds")
+fit_1f_orth2 = readRDS(paste0(path, "/data_fit_1f_orth2.Rds"))
 object = to_list(fit_1f_orth2)
 expect_equal(reliability(object, type = "sumscore",
                          iota = rep(1, 9))$reliability,
